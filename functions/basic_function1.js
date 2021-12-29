2ba0b626d74ab6b9a4b3b4c7dc3c5c2bd1ca73f6
Andre Almeida <andre-silva78@hotmail.com>
2021-12-29 00:18:01 -0300

    Basic function declarations

---
+++ functions/basic_function1.js
@@ -0,0 +1,26 @@
+// Function Declaration
+function goodDay() {
+  console.log("Good Day!");
+}
+
+goodDay()
+
+// Function Expression
+const goodEvening = function () {
+  console.log('Good Evening!');
+}
+
+goodEvening()
+
+function sum(a, b) {
+  return a + b
+}
+
+let result = sum(1, 2)
+console.log(result)
+
+result = sum(3, 5)
+console.log(result)
+
+result = sum(10)
+console.log(result)
