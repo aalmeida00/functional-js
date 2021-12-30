efffb88d72c5d5808964154b9cf0dd74444d4924
Andre Almeida <andre-silva78@hotmail.com>
2021-12-30 19:44:29 -0300

    Callback using map  basic

---
+++ functions/callback_3.js
@@ -0,0 +1,13 @@
+const nums = [1,2,3,4,5]
+const double = nums.map(n => console.log(n * 2))
+
+const names = ['John', 'Amy', 'Mary']
+const firstLetter = names.map(n => console.log(n[0]))
+
+const cart = [
+  { name: 'iPhone', qtty: 3, price: 1000 },
+  { name: 'MacBook', qtty: 1, price: 2000 },
+  { name: 'MacBook Pro', qtty: 6, price: 3000 },
+  { name: 'iPad', qtty: 2, price: 500 },
+  { name: 'iMac', qtty: 6, price: 4000 },
+]
